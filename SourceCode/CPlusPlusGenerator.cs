d9201e2334f055795aabc322f110df1f4a0968bf
Stephen Punak <stephen.punak@lordstownmotors.com>
2022-05-10 15:51:19 -0700

    (no branch):
    Add source class

---
+++ SourceCode/CPlusPlusGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanoopCommon.SourceCode
+{
+	public class CPlusPlusGenerator : SourceGenerator
+	{
+		public override bool GenerateEnum(string name, Set values)
+		{
+			BeginEnum(name);
+			foreach(var value in values)
+			{
+				if(value.Value != null)
+					IndentWrite($"{value.Key} = {value.Value},");
+				else
+					IndentWrite($"{value.Key},");
+				NewLine();
+			}
+			EndEnum();
+			return true;
+		}
+
+		private void BeginEnum(String name)
+		{
+			IndentWrite($"enum {name}");
+			NewLine();
+			OpenCurlyBrace();
+		}
+
+		private void EndEnum()
+		{
+			CloseCurlyBrace();
+			NewLine();
+		}
+	}
+}
---
+++ SourceCode/SourceGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanoopCommon.SourceCode
+{
+	public abstract class SourceGenerator
+	{
+		public class Set : Dictionary<String, Object> { }
+
+		public abstract bool GenerateEnum(String name, Set values);
+
+		private int _indentLevel;
+
+		protected int IndentLevel 
+		{
+			get { return _indentLevel; }
+			set { if(_indentLevel >= 0) _indentLevel = value; }
+		}
+
+		public int IndentSize { get; set; }
+		protected StringWriter Writer { get; set; }
+
+		protected SourceGenerator()
+		{
+			Writer = new StringWriter();
+			IndentLevel = 0;
+			IndentSize = 4;
+		}
+
+		protected void IndentWrite(String value)
+		{
+			for(int i = 0;i < IndentLevel * IndentSize;i++)
+				Writer.Write(' ');
+			Writer.Write(value);
+		}
+
+		protected void NewLine()
+		{
+			Writer.Write("\n");
+		}
+
+		protected void OpenCurlyBrace()
+		{
+			IndentWrite("{");
+			NewLine();
+			IndentLevel++;
+		}
+
+		protected void CloseCurlyBrace()
+		{
+			IndentLevel--;
+			IndentWrite("};");
+			NewLine();
+		}
+
+		public override string ToString()
+		{
+			return Writer.ToString();
+		}
+	}
+}
